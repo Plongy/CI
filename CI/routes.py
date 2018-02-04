d661b35019bd728dd77fb129f6dd935dd213fde7
Alexander Viklund <viklu@kth.se>
2018-02-04 01:55:49 +0000

    Feat: Add route for handling github webhooks, close #3

--- CI/routes.py
+++ CI/routes.py
@@ -1,7 +1,57 @@
+import os
+import shutil
+
+from flask import request
+
 from CI import app
+from CI.CI_helpers import *
+from CI.constants import CLONE_FOLDER, CONF
 
 
 @app.route('/')
 def index():
     """The root endpoint, will not be used in the project, but is good for checking setups"""
     return 'Hello, World!'
+
+
+@app.route('/hooks/github', methods=['POST'])
+def github_webhook():
+    data = request.get_json()
+    print(data)
+
+    head_sha = data['head_commit']['id']
+    repo_data = data['repository']
+
+    # noinspection PyBroadException
+    try:
+        set_commit_state(repo_data['full_name'], head_sha, "pending")
+
+        os.makedirs(CLONE_FOLDER + repo_data['full_name'], exist_ok=True)
+        clone_repo(repo_data['ssh_url'], f"{CLONE_FOLDER}{repo_data['full_name']}/{head_sha}")
+
+        config = read_configfile(f"{CLONE_FOLDER}{repo_data['full_name']}/{head_sha}/{CONF}")
+
+        # Change to repo folder
+        main_dir = os.getcwd()
+        os.chdir(f"{CLONE_FOLDER}{repo_data['full_name']}/{head_sha}")
+
+        command_results = run_commands(config['commands'])
+
+        # Return to root folder
+        os.chdir(main_dir)
+        shutil.rmtree(f"{CLONE_FOLDER}{repo_data['full_name']}/{head_sha}", ignore_errors=True)
+
+        successful_commands = [is_successful_command(*pair) for pair in zip(command_results, config['success_strings'])]
+
+        set_commit_state(
+            repo_data['full_name'],
+            head_sha,
+            "success" if all(successful_commands) else "failure"
+        )
+    except Exception:
+        set_commit_state(
+            repo_data['full_name'],
+            head_sha,
+            "failure"
+        )
+    return ""  # We have to return something
