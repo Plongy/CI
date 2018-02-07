4010d884796e77c7e27a3d725a13bd7933387ff1
Alexander Viklund <viklu@kth.se>
2018-02-07 04:48:34 +0100

    Refactor: Make sure to follow PEP8 and pyflakes

--- CI/constants.py
+++ CI/constants.py
@@ -1,7 +1,8 @@
 import os
 
-CLONE_FOLDER = os.getenv('CLONE_FOLDER', 'tmp/')
-HISTORY_FOLDER = os.getenv('HISTORY_FOLDER', 'history/')
-OAUTH_TOKEN = os.getenv('OAUTH_TOKEN', '1234567890123456789012345678901234567890')
+# ignore some line length errors
+CLONE_FOLDER = os.getenv('CLONE_FOLDER', 'tmp/')  # NOQA
+HISTORY_FOLDER = os.getenv('HISTORY_FOLDER', 'history/')  # NOQA
+OAUTH_TOKEN = os.getenv('OAUTH_TOKEN', '1234567890123456789012345678901234567890')  # NOQA
 
 CONF = 'ci_conf.json'
--- CI/routes.py
+++ CI/routes.py
@@ -4,14 +4,17 @@ import shutil
 from flask import request
 
 from CI import app
-from CI.CI_helpers import *
+from CI.CI_helpers import \
+    set_commit_state, clone_repo, read_configfile, \
+    run_commands, is_successful_command
 from CI.constants import CLONE_FOLDER, CONF
 from CI.history_helpers import log_process
 
 
 @app.route('/')
 def index():
-    """The root endpoint, will not be used in the project, but is good for checking setups"""
+    """The root endpoint, will not be used in the project,
+    but is good for checking setups"""
     return 'Hello, World!'
 
 
@@ -27,10 +30,18 @@ def github_webhook():
     try:
         set_commit_state(repo_data['full_name'], head_sha, "pending")
 
-        os.makedirs(CLONE_FOLDER + repo_data['full_name'], exist_ok=True)
-        clone_repo(repo_data['ssh_url'], f"{CLONE_FOLDER}{repo_data['full_name']}/{head_sha}")
+        os.makedirs(
+            CLONE_FOLDER + repo_data['full_name'],
+            exist_ok=True
+        )
+        clone_repo(
+            repo_data['ssh_url'],
+            f"{CLONE_FOLDER}{repo_data['full_name']}/{head_sha}"
+        )
 
-        config = read_configfile(f"{CLONE_FOLDER}{repo_data['full_name']}/{head_sha}/{CONF}")
+        config = read_configfile(
+            f"{CLONE_FOLDER}{repo_data['full_name']}/{head_sha}/{CONF}"
+        )
 
         # Change to repo folder
         main_dir = os.getcwd()
@@ -40,9 +51,15 @@ def github_webhook():
 
         # Return to root folder
         os.chdir(main_dir)
-        shutil.rmtree(f"{CLONE_FOLDER}{repo_data['full_name']}/{head_sha}", ignore_errors=True)
+        shutil.rmtree(
+            path=f"{CLONE_FOLDER}{repo_data['full_name']}/{head_sha}",
+            ignore_errors=True
+        )
 
-        successful_commands = [is_successful_command(*pair) for pair in zip(command_results, config['success_strings'])]
+        successful_commands = [
+            is_successful_command(*pair)
+            for pair in zip(command_results, config['success_strings'])
+        ]
 
         set_commit_state(
             repo_data['full_name'],
@@ -56,10 +73,11 @@ def github_webhook():
             command_output=command_results,
             webhook_json=data
         )
-    except Exception:
+    except Exception as e:
         set_commit_state(
             repo_data['full_name'],
             head_sha,
             "failure"
         )
+        print(e)
     return ""  # We have to return something
